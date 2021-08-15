c5b4705cd07bdc212f02065036b94afdcb329805
Daniel Baker <dbaker731@gmail.com>
2021-08-15 00:42:52 -0500

    init commit

---
+++ BSBracketsApp/BSLogic/BSLogic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSLogic
+{
+    public class BSLogicClass
+    {
+        public BSLogicClass() { }
+    }
+}
---
+++ BSBracketsApp/Repo/Context.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repo
+{
+    public class Context : DbContext
+    {
+    }
+}
